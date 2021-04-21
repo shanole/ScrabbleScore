973ffda22f84cef335325f8ff04e59c7b6ca8a1d
shanole <73411129+shanole@users.noreply.github.com>
2021-04-20 19:29:52 -0700

    Initial commit
    Co-authored-by: Cat Denton <willwdenton@gmail.com>
    Co-authored-by: Karlson Drendel <kdrendel99@gmail.com>

---
+++ ScrabbleScore.Tests/ModelTests/WordTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ScrabbleScore.Models;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class WordTests
+  {
+
+  }
+}
